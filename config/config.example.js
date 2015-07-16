36739321a4892c1385effc0c18b50cb22b560e1f
Eric Allen <decoy@internetalche.me>
2015-07-16 19:37:00 -0400

    switched to blank config.example.js file

---
+++ config/config.example.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var alchemyConfig = {
+    disqus_shortname: "",
+    google_analytics_id: "",
+    google_webmaster_tools: "",
+    homepage_title: "",
+    mailchimp_action_url: "",
+    posted_by_text: "Written by",
+    show_author_in_preview: true,
+    show_blow_name_in_title: true,
+    social_media_links: [],
+    sub_title: "",
+    syntax_highlighting: true,
+    syntax_theme: 'github',
+    syntax_theme_custom: false,
+    title_separator: '|',
+    use_relative_date: true,
+    skin: 'alchemy'
+};
+
+module.exports = alchemyConfig;
